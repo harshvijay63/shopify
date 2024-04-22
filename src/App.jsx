11b6a3b8c7c5e61817641ef373c3d564ca34fe3a
Harsh Vijay <72139720+harshvijay63@users.noreply.github.com>
2024-04-22 08:34:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { Outlet } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Home from './pages/home/Home';
 
 function App() {
 
   return (
     <>
       <Navbar/>
-      <Outlet /> 
+      <Home />
       <Footer />
     </>
   )
